8a4863245964613cdce034d5523bf273363cbbea
Manikandanraj <manikandan.raj0@gmail.com>
2021-04-15 19:00:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker build -t ghcr.io/maniraj007/test-docker --cache-from=ghcr.io/maniraj007/test-docker
+docker build -t ghcr.io/maniraj007/test-docker --cache-from=ghcr.io/maniraj007/test-docker ./DockerImageTest
 docker push ghcr.io/maniraj007/test-docker
 docker run ghcr.io/maniraj007/test-docker
