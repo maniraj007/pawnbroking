bc3119c6627396d437d0e7fd37a2c78477bb7147
Manikandanraj <manikandan.raj0@gmail.com>
2021-04-15 18:58:23 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker-compose build service1
-docker-compose push service1
-docker-compose run service1
+docker build -t ghcr.io/maniraj007/test-docker --cache-from=ghcr.io/maniraj007/test-docker
+docker push ghcr.io/maniraj007/test-docker
+docker run ghcr.io/maniraj007/test-docker
